b976b55ad0cbc431abbc284a1a5121c7f537fee7
lakshaykaushik051 <lakshaykaushik051@gmail.com>
2025-04-30 11:57:16 +0000

    working_on_hyde

--- chatbot/llm_calling.py
+++ chatbot/llm_calling.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from prompts import choose_rag_or_not_system_prompt
+from prompts import choose_rag_or_not_system_prompt, multiple_query_system_prompt
 import json
 
 load_dotenv()
@@ -25,3 +25,22 @@ def llm_or_rag(query):
 
     except Exception as err:
         print(f"Error in llm_or_rag() :::: {err}")
+
+
+def multiple_query_generator(query):
+    try:
+        client = llm_client("gemini")
+        messages = [
+            {'role':'system','content':multiple_query_system_prompt},
+            {'role':'user','content':'query'}
+        ]
+        response = client.chat.completeions.create(
+            model='',
+            response_format={'type':'json_object'},
+            messages=messages
+        )
+        parsed_output = json.loads(response.choices[0].messages.content)
+        return parsed_output
+    except Exception as err:
+        print(f"Error in multiple_query_generator() :::: {err}")
+        return []
--- chatbot/prompts.py
+++ chatbot/prompts.py
@@ -21,7 +21,25 @@ Examples:
 
 Now evaluate the actual query below.
 
-Query: "{user_query}"
+Query: "{{user_query}}"
 RAG Document Description: "{rag_doc_description}"
 
+'''
+
+
+multiple_query_system_prompt = f'''
+You are a Parallel Query Decomposition Engine. Your primary function is to analyze a single, incoming user query and break it down into a set of distinct, independent sub-queries. These sub-queries must be suitable and optimized for execution in parallel across multiple data sources or processing nodes.
+
+Goal: Generate a list of sub-queries that, when executed concurrently and their results combined, will fulfill the intent of the original user query.
+
+Output Format: Provide the decomposed sub-queries as a structured list (e.g., a JSON array of strings), where each string represents a single sub-query ready for execution.
+
+Example Input : What is cpython? explain with examples
+
+Example Output (Conceptual):
+[
+  "What is cpython?",
+  "Cpython with example",
+  "Use of cpython"
+]
 '''
