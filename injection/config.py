9a92e0bfd4e69c15b9c7f01e2364a9b4bdb5517a
lakshay <lakshaykaushik051@gmail.com>
2025-05-04 15:29:13 +0530

    injection_debugging

---
+++ injection/config.py
@@ -0,0 +1,5 @@
+CHAT_MODEL="gemini-2.5-pro-preview-03-25"
+
+RAG_DESCRIPTION = "This Docs contains Documents for prgramming from website chaianddocs.com example C++,Python etc"
+
+EMBEDDING_CHAT_MODEL="text-embedding-3-large"
---
+++ injection/vector_db.py
@@ -0,0 +1,71 @@
+from langchain_qdrant import QdrantVectorStore, RetrievalMode
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from config import EMBEDDING_CHAT_MODEL
+import os
+
+
+load_dotenv()
+
+
+client = QdrantClient(
+    api_key = os.getenv('QDRANT_API_KEY'),
+    url = os.getenv('QDRANT_URL')
+)
+
+def add_data(collection_name,chunks_data):
+    try:
+        
+        collection_names = [c.name for c in client.get_collections().collections]
+        check_collection_exist = collection_name in collection_names
+
+        print(check_collection_exist)
+
+        if not check_collection_exist:
+            client.create_collection(
+                collection_name=collection_name,
+                vector_config = VectorParams(size=3072,distance = Distance.COSINE)
+            )
+        
+        QdrantVectorStore.add_documents(
+            documents = chunks_data,
+            collection_name = collection_name,
+            embedding = OpenAIEmbeddings(model=EMBEDDING_CHAT_MODEL,api_key=os.getenv('OPENAI_KEY')),
+            client = client
+        )
+        print(f'''{'-'*40}INJECTION-DONE{'-'*40}''')
+        return "Success"
+
+    except Exception as err:
+        print(f"Error in add_data() :::: {err}")
+        return "Error"
+
+
+def get_data(collection_name,query):
+    try:
+        vector_store = QdrantVectorStore(
+            client=client,
+            collection_name=collection_name,
+            embedding=OpenAIEmbeddings(model=EMBEDDING_CHAT_MODEL,api_key=os.getenv('OPENAI_KEY')),
+            retrieval_mode = RetrievalMode.DENSE
+        )
+
+        return vector_store.similarity_search(query)
+
+        
+        
+    except Exception as err:
+        print(f"Error in get_data() :::: {err}")
+        return []
+    
+
+def get_all_collections():
+    try:
+        coll = []
+        coll = client.get_collections()
+        return coll
+    except Exception as err:
+        print(f"Error in avaliable_collections() :::: {err}")
+        return []
